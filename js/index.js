11999ec718af546d4579373aed0a778460116305
Zac Willmington <zac.willmington@gmail.com>
2018-09-12 20:30:09 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// use this to copy code snippets or us your browser's console
+
+function greet(customerOne, customerTwo) {
+	console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+
+let sally = { name: 'Sally' };
+
+greet.apply(sally);
+// my name is Sally, hi!
+
+greet.call(sally);
