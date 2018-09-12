fdfa4acd9bb33f07da7a12c1424c3c2fc2ee51cb
Zac Willmington <zac.willmington@gmail.com>
2018-09-12 20:36:16 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 
-function greet(customerOne, customerTwo) {
-	console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
-}
-
 let sally = { name: 'Sally' };
 
+function greet(customer) {
+	console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
 
-greet.call(sally, 'Terry', 'George');
+let newGreet = greet.bind(sally);
 
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+
+greet('Bob');
+// Hi Bob, my name is !
